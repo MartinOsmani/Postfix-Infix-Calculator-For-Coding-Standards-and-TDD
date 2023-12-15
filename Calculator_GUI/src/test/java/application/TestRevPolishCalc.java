cb2233d5133354683bd2088a176aa31e104d43de
Martin Osmani <wlis209@live.rhul.ac.uk>
2023-12-15 03:42:44 +0000

    Added the first instance of RevPolishCalc using TDD.

---
+++ Calculator_GUI/src/test/java/application/TestRevPolishCalc.java
@@ -0,0 +1,14 @@
+package application;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class TestRevPolishCalc {
+
+  @Test
+  void testRevPolishCalc() {
+    RevPolishCalc rvc = new RevPolishCalc(); // 1st Test fixed by adding RevPolishCalc Class
+  }
+  
+  
+}
